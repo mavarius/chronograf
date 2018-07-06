b0db189b3369b454f83a9e0afd43bbc689e61ed6
Michael Desa <mjdesa@gmail.com>
2018-07-06 12:54:35 -0700

    feat(bolt): implement cell service on bolt client

--- bolt/client.go
+++ bolt/client.go
@@ -161,6 +161,10 @@ func (c *Client) initialize(ctx context.Context) error {
 		if _, err := tx.CreateBucketIfNotExists(MappingsBucket); err != nil {
 			return err
 		}
+
+		// Always create Cells bucket.
+		if err := c.initializeCells(ctx, tx); err != nil {
+		}
 		return nil
 	}); err != nil {
 		return err
